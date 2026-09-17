9da7cb4e3ac67a3deb53ae009d48569b616edf50
Anthony Tornetta <cornchipgonecodin@gmail.com>
2026-09-17 12:44:41 -0400

    env testing

--- .hermes/functions/hello.js
+++ .hermes/functions/hello.js
@@ -2,11 +2,12 @@ exports.handler = async (request) => {
   const query = new URLSearchParams(request.query ?? "");
   const name = query.get("name") ?? "world";
 
+
   return {
     status: 200,
     headers: { "content-type": "application/json" },
     body: {
-      message: `Hello, ${name}!`,
+      message: JSON.stringify(process.env),
       method: request.method
     }
   };
