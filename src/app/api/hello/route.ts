88d829dc443619d2379a92913f19697c4d93242c
Anthony Tornetta <cornchipgonecodin@gmail.com>
2026-09-22 19:35:04 -0400

    log env

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,5 +1,6 @@
 export async function GET() {
   console.log('Next js route run!');
+  console.log(process.env);
   return Response.json({
     message: "Hello from the API! (Webhook Worked)",
     timestamp: new Date().toISOString(),
