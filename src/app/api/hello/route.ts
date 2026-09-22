5d41f6222d4279d592d745133912e6beb2c10f5c
Anthony Tornetta <cornchipgonecodin@gmail.com>
2026-09-22 19:33:00 -0400

    Adding log to API route

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,4 +1,5 @@
 export async function GET() {
+  console.log('Next js route run!');
   return Response.json({
     message: "Hello from the API! (Webhook Worked)",
     timestamp: new Date().toISOString(),
